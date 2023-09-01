f484a96e7f15f035f0d051e6e90679f1e4950a44
Davi (Estudo) <daviamorim_sz@live.com>
2023-09-01 00:45:49 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+// const passVisibilityIcon = document.getElementById('userPass__Icon');
+// const inputUserPass = document.getElementById('userPass')
+
+// passVisibilityIcon.addEventListener('click', togglePassVisibility);
+
+// function togglePassVisibility() {
+
+//     if (passVisibilityIcon.classList.contains('ph-eye')) {
+//         addAndRemoveClass('ph-eye-slash', 'ph-eye');
+//         inputUserPass.type = 'password';
+
+//     } else {
+//         addAndRemoveClass('ph-eye', 'ph-eye-slash');
+//         inputUserPass.type = 'text';
+//     }
+// }
+
+// function addAndRemoveClass(addClass, removeClass) {
+//     passVisibilityIcon.classList.add(`${addClass}`);
+//     passVisibilityIcon.classList.remove(`${removeClass}`);
+// }
