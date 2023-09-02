5940e23f4a5f7fb56f8fa6e775acc0080ee2c4d7
Davi (Estudo) <daviamorim_sz@live.com>
2023-09-02 01:38:19 -0300

    refactor-script

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,22 +3,22 @@ const form = document.getElementById('form');
 const submitButton = document.getElementById('submit');
 
 
-inputs[0].addEventListener('keyup', nameValidate);
+validateField(0, nameValidate);
 
 function nameValidate() {
-    const nameFieldValue = inputs[0].value
+    const nameFieldValue = inputs[0].value;
 
-    nameFieldValue.length < 2 ? fieldActions(0, 'block', '#F67669') : fieldActions(0, 'none', '#E6E6F0')
+    nameFieldValue.length < 2 ? fieldActions(0, 'block', '#F67669') : fieldActions(0, 'none', '#E6E6F0');
 
     if (nameFieldValue.trim() === '') {
         fieldActions(0, 'none', '#E6E6F0');
     }
 }
 
-inputs[1].addEventListener('keyup', surnameValidate);
+validateField(1, surnameValidate);
 
 function surnameValidate() {
-    const surnameFieldValue = inputs[1].value
+    const surnameFieldValue = inputs[1].value;
 
     surnameFieldValue.length < 4 ? fieldActions(1, 'block', '#F67669') : fieldActions(1, 'none', '#E6E6F0')
 
@@ -27,8 +27,26 @@ function surnameValidate() {
     }
 }
 
+validateField(2, emailValidate);
+
+function emailValidate() {
+    const emailFieldValue = inputs[2].value;
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    emailRegex.test(emailFieldValue) ? fieldActions(2, 'none', '#E6E6F0') : fieldActions(2, 'block', '#F67669');
+
+    if (emailFieldValue.trim() === '') {
+        fieldActions(2, 'none', '#E6E6F0');
+    }
+}
+
 // Função para alterar o display e borda dos elementos dinamicamente
 function fieldActions(index, displayValue, borderColor) {
     inputs[index].nextElementSibling.style.display = displayValue;
     inputs[index].style.border = '1px solid' + borderColor;
+}
+
+// Função responsável por chamar o evento 'keyup' nos campos
+function validateField(index, func) {
+    inputs[index].addEventListener('keyup', func);
 }
