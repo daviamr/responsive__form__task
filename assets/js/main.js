e74cb7dd3600d947d04e7d80d4bda8057c17e1e3
Davi (Estudo) <daviamorim_sz@live.com>
2023-09-02 01:17:17 -0300

    refactor-script

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,42 +6,29 @@ const submitButton = document.getElementById('submit');
 inputs[0].addEventListener('keyup', nameValidate);
 
 function nameValidate() {
-    const nameField = inputs[0];
     const nameFieldValue = inputs[0].value
-    const spanElement = inputs[0].nextElementSibling;
 
-    if (nameFieldValue.length < 3) {
-        fieldActions(spanElement, 'block', nameField, '#F67669');
-    } else {
-        fieldActions(spanElement, 'none', nameField, '#E6E6F0');
-    }
-    if (nameFieldValue === '') {
-        fieldActions(spanElement, 'none', nameField, '#E6E6F0');
-    }
+    nameFieldValue.length < 2 ? fieldActions(0, 'block', '#F67669') : fieldActions(0, 'none', '#E6E6F0')
 
+    if (nameFieldValue.trim() === '') {
+        fieldActions(0, 'none', '#E6E6F0');
+    }
 }
 
 inputs[1].addEventListener('keyup', surnameValidate);
 
 function surnameValidate() {
-    const surnameField = inputs[1];
     const surnameFieldValue = inputs[1].value
-    const spanElement = inputs[1].nextElementSibling;
 
-    if (surnameFieldValue.length < 5) {
-        fieldActions(spanElement, 'block', surnameField, '#F67669');
-    } else {
-        fieldActions(spanElement, 'none', surnameField, '#E6E6F0');
-    }
-    if (surnameFieldValue === '') {
-        fieldActions(spanElement, 'none', surnameField, '#E6E6F0');
-    }
+    surnameFieldValue.length < 4 ? fieldActions(1, 'block', '#F67669') : fieldActions(1, 'none', '#E6E6F0')
 
+    if (surnameFieldValue.trim() === '') {
+        fieldActions(1, 'none', '#E6E6F0');
+    }
 }
 
 // Função para alterar o display e borda dos elementos dinamicamente
-function fieldActions(spanDisplay, displayValue, fieldEdge, hexadecimal) {
-    spanDisplay.style.display = displayValue;
-    fieldEdge.style.border = '1px solid' + hexadecimal;
+function fieldActions(index, displayValue, borderColor) {
+    inputs[index].nextElementSibling.style.display = displayValue;
+    inputs[index].style.border = '1px solid' + borderColor;
 }
-
