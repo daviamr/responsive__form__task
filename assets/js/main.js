6a310b1bd0d4a380794511d132df9f527356d42b
Davi (Estudo) <daviamorim_sz@live.com>
2023-09-01 21:32:28 -0300

    validations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,47 @@
-// const passVisibilityIcon = document.getElementById('userPass__Icon');
-// const inputUserPass = document.getElementById('userPass')
+const inputs = document.getElementsByClassName('required')
+const form = document.getElementById('form');
+const submitButton = document.getElementById('submit');
 
-// passVisibilityIcon.addEventListener('click', togglePassVisibility);
 
-// function togglePassVisibility() {
+inputs[0].addEventListener('keyup', nameValidate);
 
-//     if (passVisibilityIcon.classList.contains('ph-eye')) {
-//         addAndRemoveClass('ph-eye-slash', 'ph-eye');
-//         inputUserPass.type = 'password';
+function nameValidate() {
+    const nameField = inputs[0];
+    const nameFieldValue = inputs[0].value
+    const spanElement = inputs[0].nextElementSibling;
 
-//     } else {
-//         addAndRemoveClass('ph-eye', 'ph-eye-slash');
-//         inputUserPass.type = 'text';
-//     }
-// }
+    if (nameFieldValue.length < 3) {
+        fieldActions(spanElement, 'block', nameField, '#F67669');
+    } else {
+        fieldActions(spanElement, 'none', nameField, '#E6E6F0');
+    }
+    if (nameFieldValue === '') {
+        fieldActions(spanElement, 'none', nameField, '#E6E6F0');
+    }
 
-// function addAndRemoveClass(addClass, removeClass) {
-//     passVisibilityIcon.classList.add(`${addClass}`);
-//     passVisibilityIcon.classList.remove(`${removeClass}`);
-// }
+}
+
+inputs[1].addEventListener('keyup', surnameValidate);
+
+function surnameValidate() {
+    const surnameField = inputs[1];
+    const surnameFieldValue = inputs[1].value
+    const spanElement = inputs[1].nextElementSibling;
+
+    if (surnameFieldValue.length < 5) {
+        fieldActions(spanElement, 'block', surnameField, '#F67669');
+    } else {
+        fieldActions(spanElement, 'none', surnameField, '#E6E6F0');
+    }
+    if (surnameFieldValue === '') {
+        fieldActions(spanElement, 'none', surnameField, '#E6E6F0');
+    }
+
+}
+
+// Função para alterar o display e borda dos elementos dinamicamente
+function fieldActions(spanDisplay, displayValue, fieldEdge, hexadecimal) {
+    spanDisplay.style.display = displayValue;
+    fieldEdge.style.border = '1px solid' + hexadecimal;
+}
+
